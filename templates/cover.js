8c1c1948bd091df4285f09c29a876ee57956d3fc
Saurabh <saurabhdave8@gmail.com>
2026-07-25 11:22:10 +0530

    Restore original logo and CSS mask to make it visible

--- templates/cover.js
+++ templates/cover.js
@@ -20,7 +20,7 @@ const generateCoverPage = (monthYear) => {
     <div class="cover-header">
       <div class="cover-top-bar">
         <div class="brand-logo-container">
-          <img src="${pureTreeLogo}" alt="Pure Tree Logo" class="brand-logo-img" />
+          <div class="brand-logo-img" style="-webkit-mask-image: url('${pureTreeLogo}'); mask-image: url('${pureTreeLogo}');"></div>
           <span class="organic-pill">100% CERTIFIED ORGANIC</span>
         </div>
         <div class="price-list-box">
